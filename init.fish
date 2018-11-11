6e04cd31899c70e0966ea8e7428279eed4223db3
Barbz <BarbzYHOOL@users.noreply.github.com>
2018-11-11 17:07:39 +0100

    Add missing commands from official pyenv install

--- init.fish
+++ init.fish
@@ -1,3 +1,5 @@
 set -q PYENV_ROOT; or set -l PYENV_ROOT $HOME/.pyenv
 
 set PATH $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH
+status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (pyenv virtualenv-init -|psub)
